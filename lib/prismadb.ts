5dd4b69466ec021b0164d276c366309733ac2009
MamunAG <mamun166036@yahoo.com>
2024-11-25 10:20:03 +0600

    Update prismadb.ts

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,11 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-const prismadb = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient()
 
-export default prismadb;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
