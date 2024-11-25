1d836acdf13c2ecd5b1ff5f92050f1fd2c296b7d
MamunAG <mamun166036@yahoo.com>
2024-11-25 07:01:18 +0600

    Update route.ts

--- app/api/tag/route.ts
+++ app/api/tag/route.ts
@@ -1,8 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
 import prismadb from "@/lib/prismadb";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(
+  req: NextRequest
+) {
   const tag = await prismadb.tag.findMany();
+
+  if (!tag) {
+    NextResponse.json({ message: "Tag not found." }, { status: 404 });
+  }
   return NextResponse.json(tag);
 }
