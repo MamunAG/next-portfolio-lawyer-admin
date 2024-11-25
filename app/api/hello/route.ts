8c986568eeabf4170fc394259e301f6734a35691
MamunAG <mamun166036@yahoo.com>
2024-11-25 07:07:21 +0600

    Delete app/api/hello directory

--- app/api/hello/route.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextRequest, NextResponse } from "next/server";
-
-export function GET(req: NextRequest, res: NextResponse) {
-  return NextResponse.json({ kkk: "dsaf" });
-}
